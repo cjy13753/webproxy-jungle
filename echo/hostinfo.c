2a9e3b257219faede92740798c54066bfc8b343a
JunYoung Choi <cjy13753@gmail.com>
2021-12-18 22:33:18 +0900

    Get hands dirty with hostinfo.c to get used to getaddrinfo and getnameinfo functions

---
+++ echo/hostinfo.c
@@ -0,0 +1,36 @@
+#include "csapp.h"
+
+int main(int argc, char **argv)
+{
+    struct addrinfo *p, *listp, hints;
+    char host[MAXLINE];
+    char service[MAXLINE];
+    int rc, flags;
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <domain name>\n", argv[0]);
+        exit(0);
+    }
+
+    /* Get a list of addrinfo records */
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET; /* IPv4 only */
+    hints.ai_socktype = SOCK_STREAM; /* Connections only */
+    if ((rc = getaddrinfo(argv[1], "80", &hints, &listp)) != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(rc));
+    }
+
+    /* Walk the list and display each IP address */
+    flags = NI_NUMERICHOST; /* Display address string instead of domain name */
+    flags |= NI_NUMERICSERV;
+    for (p = listp; p; p = p->ai_next) {
+        Getnameinfo(p->ai_addr, p->ai_addrlen, host, MAXLINE, service, MAXLINE, flags);
+        printf("%s\n", host);
+        printf("%s\n", service);
+    }
+
+    /* Clean up */
+    Freeaddrinfo(listp);
+
+    exit(0);
+}
